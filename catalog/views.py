e72d9217058fb235e07da246d2b902fbcfba8fa2
sergei valdemarov <valdemarovsergei@gmail.com>
2023-10-25 17:31:11 +0300

    commit912

---
+++ catalog/views.py
@@ -0,0 +1,112 @@
+from django.core.cache import cache
+
+from catalog.services import get_categories_list
+from config import settings
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import Http404
+from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, View, CreateView, UpdateView
+
+from catalog.forms import ProductForm, VersionForm
+from catalog.models import Product, Category, Version
+
+
+class HomeListView(ListView):
+    model = Category
+    template_name = 'catalog/home.html'
+
+    def get_context_data(self, **kwargs):
+        context_data = super().get_context_data(**kwargs)
+
+        categories_list = get_categories_list()
+
+        context_data['categories'] = categories_list
+        return context_data
+
+
+class ContactView(View):
+    def get(self, request):
+        context = {
+            'title': 'Контакты'
+        }
+        return render(request, 'catalog/contacts.html', context)
+
+    def post(self, request):
+        if request.method == 'POST':
+            name = request.POST.get('name')
+            phone = request.POST.get('phone')
+            message = request.POST.get('message')
+            print(f'Имя: {name}\n'
+                  f'Номер телефона: {phone}\n'
+                  f'Сообщение: {message}')
+        context = {
+            'title': 'Контакты'
+        }
+        return render(request, 'catalog/contacts.html', context)
+
+
+class ProductListView(ListView):
+    model = Product
+    template_name = 'catalog/products.html'
+    context_object_name = 'products'
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        queryset = queryset.filter(category_of_product=self.kwargs.get('pk'))
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        context_data = super().get_context_data(**kwargs)
+
+        category_item = Category.objects.get(pk=self.kwargs['pk'])
+        context_data['title'] = category_item.name_of_category
+
+        for product in context_data['products']:
+            version = product.version_set.first()
+            product.version = version
+
+        return context_data
+
+
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = 'catalog/info_about_product.html'
+    context_object_name = 'product'
+
+
+class ProductCreateView(LoginRequiredMixin, CreateView):
+    model = Product
+    form_class = ProductForm
+    success_url = reverse_lazy('catalog:home')
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+
+class ProductUpdateView(LoginRequiredMixin, UpdateView):
+    model = Product
+    form_class = ProductForm
+    success_url = reverse_lazy('catalog:home')
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse_lazy('catalog:product_detail', args=[self.object.pk])
+
+
+class VersionCreateView(LoginRequiredMixin, CreateView):
+    model = Version
+    form_class = VersionForm
+    success_url = reverse_lazy('catalog:home')
+
+    def get_form_kwargs(self):
+        kwargs = super().get_form_kwargs()
+        kwargs['user'] = self.request.user
+        return kwargs
+
+    def form_invalid(self, form):
+        raise Http404("У вас нет разрешения создавать версии продуктов")
